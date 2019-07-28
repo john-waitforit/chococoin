4a4f0afa764ea84d77c597cfd525be1a5ac3980b
Craig Mayhew <craig@mayhew.io>
2019-07-28 14:49:52 +0100

    Setup rinkeby details from environment variables (provided by CI env)

--- source/truffle-config.js
+++ source/truffle-config.js
@@ -1,21 +1,10 @@
 module.exports = {
-  // Uncommenting the defaults below 
-  // provides for an easier quick-start with Ganache.
-  // You can also follow this format for other networks;
-  // see <http://truffleframework.com/docs/advanced/configuration>
-  // for more details on how to specify configuration options!
-  //
-  //networks: {
-  //  development: {
-  //    host: "127.0.0.1",
-  //    port: 7545,
-  //    network_id: "*"
-  //  },
-  //  test: {
-  //    host: "127.0.0.1",
-  //    port: 7545,
-  //    network_id: "*"
-  //  }
-  //}
-  //
+  networks: {
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(process.env.mnemonic, process.env.infuraurl)
+      },
+      network_id: 3
+    }
+  }
 };
